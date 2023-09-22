3bdac6a67d91e8ddcfea013ce60c24b15a9748d4
hkarani <hezronkaranii@gmail.com>
2023-09-23 02:19:35 +0300

    Removing initialised bloat files

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,15 @@
-﻿using System.Diagnostics;
-using btcTestnetCoins.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace btcTestnetCoins.Controllers
 {
-    public class HomeController : Controller
+	public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace btcTestnetCoins.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
