a0d8d48f2a4e7205a84f5c36ceaf34aa7110f7ac
hkarani <hezronkaranii@gmail.com>
2023-10-25 05:00:05 +0300

    Validate Empty and Invalid Bitcoin Address

--- Models/PayoutAddress.cs
+++ Models/PayoutAddress.cs
@@ -4,7 +4,9 @@ namespace btcTestnetCSoins.Models
 {
 	public class PayoutAddress
 	{
-		[Required]
+		[Required(ErrorMessage = "*Please enter a testnet address to receive coins")]
+		[RegularExpression(@"\b(tb(0([ac-hj-np-z02-9]{39}|[ac-hj-np-z02-9]{59})|1[ac-hj-np-z02-9]{8,87})|[mn2][a-km-zA-HJ-NP-Z1-9]{25,39})\b",
+			ErrorMessage = "*Invalid Bitcoin testnet address")]
 		public string? DestinationAddress { get; set; }
 		
 	}
