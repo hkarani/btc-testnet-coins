d4772c5da4781918de885e95623d7c40e3477977
hkarani <hezronkaranii@gmail.com>
2023-11-03 04:04:11 +0300

    Use toastr alerts from partial view

--- Models/User.cs
+++ Models/User.cs
@@ -3,11 +3,11 @@
 	public class User
 	{
 		public bool eligible;
+
 		public Guid? Id { get; set; }
 		public string? IpAddress { get; set; }
 		public DateTime? FirstTimeAccesed { get; set; }
-		public DateTime LastAccesed { get; private set; }
-		
+		public DateTime LastAccesed { get; private set; }		
 		public int NumberOfTimesAccessed { get; set; }
 		public bool IsBlocked { get; set; }
 		public bool IsEligible
@@ -16,7 +16,7 @@
 			set
 			{
 				TimeSpan timeDifference = DateTime.Now.Subtract(LastAccesed);
-				if (timeDifference.TotalDays > 3)
+				if (timeDifference.TotalDays > 2)
 				{
 					eligible = true;
 				}
